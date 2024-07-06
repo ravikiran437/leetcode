5c78069e9c51ff7a4edb97c3d705f3f1fd45bf56
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2024-07-06 10:01:30 +0530

    Time: 42 ms (63.52%) | Memory: 16.6 MB (33.22%) - LeetSync

--- 3346-lexicographically-smallest-string-after-operations-with-constraint/lexicographically-smallest-string-after-operations-with-constraint.py
+++ 3346-lexicographically-smallest-string-after-operations-with-constraint/lexicographically-smallest-string-after-operations-with-constraint.py
@@ -1,23 +1,19 @@
 class Solution:
     def getSmallestString(self, s: str, k: int) -> str:
-        p = ""
+        string = ""
         for i in s:
-            a = ord(i)-96
-            b = a-ord("a")+96
-            c = 26 - a+1
-            m = min(b,c)
-            # print(m)
-            if i == "a":
-                p += i 
-            elif k>=m:
-                p += "a"
-                k -= m
-            else:
-                if k!=0:
-                    a = ord(i)-k
-                    p += chr(a)
-                    k = 0 
-                else:
-                    p += i
-        return p 
+            num = ord(i) - 96 
+            a = num - 1 
+            b = 27 - num 
+            m = min(a,b)
+            if m <= k :
+                k -= m 
+                string += "a"
+            elif m > k :
+                string += chr(num-k+96)
+                k = 0 
+            
+
+        print(k)
+        return string
         
